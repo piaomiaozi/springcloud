30eebba80a20a8d2e901f9ec7a4fca4c458611ea
gaoyang <gaoyang@10.33.70.73>
2018-11-07 18:49:05 +0800

    update

---
+++ springcloud-zuul/src/main/java/com/gaoyang/springcloud/zuul/filter/MyFilter.java
@@ -0,0 +1,53 @@
+package com.gaoyang.springcloud.zuul.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+	private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+		Object accessToken = request.getParameter("token");
+		if (accessToken == null) {
+			log.warn("miss token......");
+			ctx.setSendZuulResponse(false);
+			ctx.setResponseStatusCode(401);
+			try {
+				ctx.getResponse().getWriter().write("miss token...");
+			} catch (Exception e) {
+			}
+			return null;
+		}
+		log.info("OK");
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
